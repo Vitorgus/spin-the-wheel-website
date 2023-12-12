98002b7364005d00dd4f8dc0ce71d0d286d0fd1f
Vitor Augusto Pinheiro <26413854+Vitorgus@users.noreply.github.com>
2023-12-12 17:45:16 -0300

    feat: banner layout without the image

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,23 +1,28 @@
 import './styles/Banner.scss'
 
+// TODO add banner image
+// TODO banner image animation
+// TODO banner wheel spin
 function Banner () {
   return (
     <section className='banner'>
-      <h1 className='banner__title'>
-        Spin to Win
-      </h1>
+      <div className='banner__wrapper'>
+        <h1 className='banner__title'>
+          Spin to Win
+        </h1>
 
-      <h3 className='banner__subtitle'>
-        Spin the wheel to win prizes
-      </h3>
+        <h3 className='banner__subtitle'>
+          Spin the wheel to win prizes
+        </h3>
 
-      <p className='banner__text'>
-        Here you can add a few sentences explaining what prizes can be won and any other information you want people to know
-      </p>
+        <p className='banner__text'>
+          Here you can add a few sentences explaining what prizes can be won and any other information you want people to know
+        </p>
 
-      <button className='banner__button'>
-        Play Now
-      </button>
+        <button className='banner__button'>
+          Play Now
+        </button>
+      </div>
     </section>
   )
 }
