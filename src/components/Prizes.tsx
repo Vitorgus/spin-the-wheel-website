28465c57238b69b6f5c6e5ea1e3d9d37b61d47ee
Vitor Augusto Pinheiro <26413854+Vitorgus@users.noreply.github.com>
2023-12-12 19:07:58 -0300

    fix: prize number formatting

--- src/components/Prizes.tsx
+++ src/components/Prizes.tsx
@@ -2,6 +2,16 @@ import './styles/Prizes.scss'
 
 import prizes from '@/data/prizes.json'
 
+function formatNumber(n: number): String {
+  let result = ''
+
+  if (n < 10) {
+    result += '0'
+  }
+
+  return result + String(n)
+}
+
 // TODO modal when clicking on prize
 function Prizes () {
   return (
@@ -17,7 +27,7 @@ function Prizes () {
 
               <img className='prizes__gallery-item-image'src={prize.imageUrl} alt={prize.imageAlt}/>
               <p className='prizes__gallery-item-tag'>
-                Prize {i+1}
+                Prize {formatNumber(i+1)}
               </p>
               <p>
                 {prize.name}
